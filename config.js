acceacafd60580867f35aacc130fadb99429625f
Teng Li <teng4@ualberta.ca>
2024-05-05 11:18:01 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
   title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
+  author: 'Teng Li', // 'Ryan Fitzgerald', // Required
+  description: 'Researcher in Robotics', // 'Full-stack Web Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://teng4.github.io', //'https://ryanfitzgerald.ca',
+    github: 'https://github.com/teng4',
+    //twitter: 'https://twitter.com/ryanafitzgerald',
+    linkedin: 'https://ca.linkedin.com/in/teng4',
   },
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://teng4.github.io/devblog/',
 };
